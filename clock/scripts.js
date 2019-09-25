675be3e66da1f69d86b1117194277635e59bfbe2
arun ravi <hello.arunravi@gmail.com>
2019-09-25 17:22:02 +0530

    analog clock dots

--- clock/scripts.js
+++ clock/scripts.js
@@ -5,7 +5,22 @@ const clock = () =>{
     var minHand= $('<span />',{ 'class' : 'minHand'});
     var hourHand= $('<span />',{ 'class' : 'hourHand'});
     var centerPoint= $('<span />',{ 'class' : 'centerPoint'});
+    var dialPointCont= $('<span />',{ 'class' : 'dialPointCont'});
     clockFrame.append(secHand, minHand, hourHand, centerPoint);
+
+    // dial points
+    for(let i = 0; i<12; i++) {
+        dialPointCont.append($('<span />', {
+            'class' : 'dialDots',
+            'style' : 'transform: rotate('+i*30+'deg) translateX(100%); transform-origin:center bottom'
+        }))
+        
+    }
+    
+    clockFrame.append(dialPointCont)
+
+
+    // clock functionalities
     let currentTime= new Date();
     let sec= currentTime.getSeconds()
     let minute= currentTime.getMinutes()
@@ -13,24 +28,24 @@ const clock = () =>{
     let secAngle = sec * 6;
     let minAngle = (minute * 6) + (.1*sec);
     let hourAngle = (hour * 30) + (minute * .5) + (.5*sec/60)
-    // let i = currentSec;
     secHand.css('transform','translateX(-50%) translateY(-50%)  rotate('+secAngle+'deg)');
     minHand.css('transform','translateX(-50%) translateY(-50%)  rotate('+minAngle+'deg)');
     hourHand.css('transform','translateX(-50%) translateY(-50%)  rotate('+hourAngle+'deg)');
-    console.log(secAngle, minAngle, hourAngle)
+    // const perform360Check = value => {value== 360? value : 0}
     setInterval(
         ()=> { 
             secAngle+= 6;
-                secAngle === 360 ? secAngle=0 : secAngle;
+            // secAngle = perform360Check(secAngle);
             minAngle+= .1;
-                minAngle === 360 ? minAngle=0 : minAngle;
+                // perform360Check(minAngle);
             hourAngle+= .5/60;
-                hourAngle === 360 ? hourAngle=0 : hourAngle
-
+                // perform360Check(hourAngle);
             secHand.css('transform','translateX(-50%) translateY(-50%)  rotate('+secAngle+'deg)');
             minHand.css('transform','translateX(-50%) translateY(-50%)  rotate('+(minAngle)+'deg)');
             hourHand.css('transform','translateX(-50%) translateY(-50%)  rotate('+(hourAngle)+'deg)');        
         } 
     , 1000);
+
+
 }
 clock()
